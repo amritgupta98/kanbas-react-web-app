f40d14e711da2d6b70f35e7ac1b81160c1d08607
amritgupta <amritgupta98@gmail.com>
2024-03-01 18:05:36 -0500

    a4

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import ParentStateComponent from "./ParentStateComponent";
+
+function sayHello() {
+  alert("Hello");
+}
+
+const Assignment4 = () => {
+  return (
+    <>
+      <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+    </>
+  );
+};
+
+export default Assignment4;
